183d67fc3a67fb6f15a84b79b38386e19054552b
Meng2222 <Dreamer_Meng2@163.com>
2018-03-27 19:33:52 +0800

    更改检测金球用光电IO口（D14）

--- Action_User/hardWare/gpio.c
+++ Action_User/hardWare/gpio.c
@@ -139,15 +139,15 @@ void PhotoelectricityCheckGoldBallInit(void)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
 	
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOE, &GPIO_InitStructure);
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
 	
-	GPIO_ResetBits(GPIOE, GPIO_Pin_12);
+	GPIO_ResetBits(GPIOD, GPIO_Pin_14);
 }
 
 static int IsBallRack=0;
