61f1e43e3f2de91804cdad1debd95f991c33fb38
suniorHYY <40032956+suniorHYY@users.noreply.github.com>
2018-08-03 15:36:55 +0800

    球

--- Action_User/control/shoot.c
+++ Action_User/control/shoot.c
@@ -81,7 +81,7 @@ void prepareMotionParaInit(void)
 {
 	/*准备射第一个球的数据*/
   PrepareShootBall1.courseAngle=167.5f;
-  PrepareShootBall1.pitchAngle=11.5f;
+  PrepareShootBall1.pitchAngle=10.5f;
   PrepareShootBall1.upSteerAngle=0.f;
 	PrepareShootBall1.downSteerAngle=-1.f;
   PrepareShootBall1.gasAim=0.52f;
@@ -104,7 +104,7 @@ void prepareMotionParaInit(void)
 	
   /*准备射第二个球的数据*/
   PrepareShootBall2.courseAngle=174.3f;
-  PrepareShootBall2.pitchAngle=8.5f;
+  PrepareShootBall2.pitchAngle=7.3f;
   PrepareShootBall2.upSteerAngle=0.0f;
 	PrepareShootBall2.downSteerAngle=-1.f;
   PrepareShootBall2.gasAim=0.49f;
@@ -119,10 +119,10 @@ void prepareMotionParaInit(void)
 	
 	/*准备射第三个球的数据*/
   PrepareShootBall3.courseAngle=178.5f;
-  PrepareShootBall3.pitchAngle=4.1f;
+  PrepareShootBall3.pitchAngle=4.5f;
   PrepareShootBall3.upSteerAngle=0.0f;
   PrepareShootBall3.downSteerAngle=-1.f;
-  PrepareShootBall3.gasAim=0.55f;
+  PrepareShootBall3.gasAim=0.554f;
 	
 	
   /*准备等待拿第三个球的数据*/
@@ -145,7 +145,7 @@ void prepareMotionParaInit(void)
   PrepareShootBall4.pitchAngle=4.f;
   PrepareShootBall4.upSteerAngle=0.0f;
   PrepareShootBall4.downSteerAngle=0.0f;
-  PrepareShootBall4.gasAim=0.522f;
+  PrepareShootBall4.gasAim=0.515f;
 	
 
 	/*准备接第四个球的参数*/
